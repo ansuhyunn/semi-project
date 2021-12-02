99729c95fa1ee303508dd51dfe53c36e308567b5
Suhyun An <unhynsua@gmail.com>
2021-12-02 18:18:16 +0900

    첫 프로젝트 배포
    첫 프로젝트 배포

---
+++ damoartProject/src/com/kh/common/JDBCTemplate.java
@@ -0,0 +1,92 @@
+package com.kh.common;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class JDBCTemplate {
+
+	public static Connection getConnection() {
+		
+		Connection conn = null;
+		
+		Properties prop = new Properties();
+		
+		String filePath = JDBCTemplate.class.getResource("/db/driver/driver.properties").getPath();
+		
+		try {
+			prop.load(new FileInputStream(filePath));
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+		try {
+			Class.forName(prop.getProperty("driver"));
+			
+			conn = DriverManager.getConnection(prop.getProperty("url"), 
+											   prop.getProperty("username"), 
+											   prop.getProperty("password"));
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+	
+	public static void commit(Connection conn) {
+		try {
+			if(conn != null && !conn.isClosed()) {
+				conn.commit();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void rollback(Connection conn) {
+		try {
+			if(conn != null && !conn.isClosed()) {
+				conn.rollback();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void close(Connection conn) {
+		try {
+			if(conn != null && !conn.isClosed()) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void close(Statement stmt) {
+		try {
+			if(stmt != null && !stmt.isClosed()) {
+				stmt.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void close(ResultSet rset) {
+		try {
+			if(rset != null && !rset.isClosed()) {
+				rset.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
