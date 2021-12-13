c086448fa607ec201b88352cea4afcfbcc190ddb
raaankim <88372177+raaankim@users.noreply.github.com>
2021-12-14 02:44:46 +0900

    첨부파일 관련 vo - 란
    첨부파일 관련 vo

---
+++ damoartProject/src/com/kh/common/model/vo/MyFileRenamePolicy.java
@@ -0,0 +1,31 @@
+package com.kh.common.model.vo;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.oreilly.servlet.multipart.FileRenamePolicy;
+
+public class MyFileRenamePolicy implements FileRenamePolicy{
+
+	@Override
+	public File rename(File originFile) {
+
+		String originName = originFile.getName();
+		
+
+		String currentTime = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+		
+
+		int ranNum = (int)(Math.random() * 90000 + 10000);
+		
+
+		String ext = originName.substring(originName.lastIndexOf("."));
+		
+		String changeName = currentTime + ranNum + ext;
+		
+		return new File(originFile.getParent(), changeName);
+	}
+
+	
+}
