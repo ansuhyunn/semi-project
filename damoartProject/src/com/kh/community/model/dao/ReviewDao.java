bc75d7dce863a5f178db3a1cc0a15cbed210ddde
nutellacherry <89463520+nutellacherry@users.noreply.github.com>
2021-12-17 00:17:02 +0900

    급한수정만.......
    수정...........

--- damoartProject/src/com/kh/community/model/dao/ReviewDao.java
+++ damoartProject/src/com/kh/community/model/dao/ReviewDao.java
@@ -8,10 +8,12 @@ import java.util.ArrayList;
 import com.kh.common.model.vo.PageInfo;
 import com.kh.community.model.vo.Review;
 
+import static com.kh.common.JDBCTemplate.*;
+
 public class ReviewDao {
 
 	public int selectListCount(Connection conn) {
-		// select문 => ResultSet (한개) => int
+		
 				int listCount = 0;
 				
 				PreparedStatement pstmt = null;
@@ -20,7 +22,7 @@ public class ReviewDao {
 				String sql = prop.getProperty("selectListCount");
 				
 				try {
-					pstmt = conn.prepareStatement(sql); // 완성된 sql문
+					pstmt = conn.prepareStatement(sql); 
 					rset = pstmt.executeQuery();
 					
 					if(rset.next()) {
@@ -39,23 +41,17 @@ public class ReviewDao {
 
 
 	public ArrayList<Review> selectList(Connection conn, PageInfo pi){
-		// select문 => ResultSet (여러행) => ArrayList<Board>
+		
 		ArrayList<Review> list = new ArrayList<>();
 		PreparedStatement pstmt = null;
 		ResultSet rset = null;
 		String sql = prop.getProperty("selectList");
 		
 		try {
-			pstmt = conn.prepareStatement(sql); // 미완성된 sql문
-			/*
-			 * currentPage : 1	=> 시작값 : 1 | 끝값 : 10 
-			 * currentPage : 2  => 시작값 : 11| 끝값 : 20
-			 * currentPage : 3  => 시작값 : 21| 끝값 : 30
-			 * 
-			 * 시작값 : (currentPage - 1) * boardLimit + 1
-			 * 끝값 : 시작값 + boardLimit - 1
-			 * 
-			 */
+			pstmt = conn.prepareStatement(sql); 
+			
+			
+			
 			int startRow = (pi.getCurrentPage() - 1) * pi.getBoardLimit() + 1;
 			int endRow = startRow + pi.getBoardLimit() - 1;
 			
